c7825c0fa573e086106012f20c08a15e134e4771
Ayesha Deshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2025-02-05 07:37:21 +0530

    Updates.........

--- Chat-App/client/src/utils/APIRoutes.js
+++ Chat-App/client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:4000";
+export const host = "https://chat-app-yhta.onrender.com"; // Render Backend URL
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
