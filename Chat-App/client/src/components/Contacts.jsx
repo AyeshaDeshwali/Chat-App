17d74a9a5438454bbb85f378b734c8ed81ec1049
Ayesha Deshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2025-02-18 08:14:43 +0530

    Update Contacts.jsx

--- Chat-App/client/src/components/Contacts.jsx
+++ Chat-App/client/src/components/Contacts.jsx
@@ -436,7 +436,8 @@ const Container = styled.div`
 
   @media screen and (max-width: 480px) {
     .current-user {
-      margin-bottom: -5.9vh;
+        margin-bottom: -8.8vh;
+      padding: 18px;
     }
     .brand h3 {
       margin-left: -48%;
